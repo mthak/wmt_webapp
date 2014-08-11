42233a59e2f61668930d4a3c229f3e998f1964ee
Manoj Thakkar <mthakkar@cloudera.com>
2014-08-11 01:15:49 -0700

    delete old data

--- start_docker.sh
+++
@@ -1,7 +0,0 @@
-ocker import wmtnode.tar wmtnode
-docker run -d  --name wmtapp -h nodeapp1.wmt.com  -p 22 -p 8080:3000  --entrypoint="/sbin/init" wmtdocker
-ipadd=`docker inspect | grep -i ipadd | cut -d":" -f2`
-echo $ipadd
-echo " run sshroot@<ipaddress above> with password nodejs  "
-echo " after logging in cd my_webapp and run node app.js"
-echo" After that you can go to the browser and run http://localhost:8080 and you will see the app running " 
